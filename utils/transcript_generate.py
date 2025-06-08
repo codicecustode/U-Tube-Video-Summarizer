0b7300bb6883ae0433fb9e0d3c43f246e7d3bcfc
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-06-08 21:32:12 +0530

    Remove redundant print statements from get_transcript function

--- utils/transcript_generate.py
+++ utils/transcript_generate.py
@@ -3,14 +3,10 @@ from youtube_transcript_api import YouTubeTranscriptApi
 def get_transcript(video_id: str):
   print(f"getting transcript for video_id: {video_id}")
   try:
-    print(f"video_id1: {video_id}")
     transcripts = YouTubeTranscriptApi.get_transcript(video_id)
     transcripted_data = ''
-    print(f"video_id2: {video_id}")
-
     for transcript in transcripts:
       transcripted_data += transcript['text'] + ' '
-    print(f"video_id2: {video_id}")
     return transcripted_data
   except Exception as e:
     print(f"an error occurred: {e}")
